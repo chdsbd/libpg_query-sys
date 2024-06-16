e29418bc3e33666c5d6ee030696495517c9d978c
Christopher Dignam <chris@dignam.xyz>
2024-06-16 16:37:48 -0400

    fix build to work across targets

--- build.rs
+++ build.rs
@@ -1,38 +1,69 @@
 extern crate bindgen;
-
+use fs_extra::dir::CopyOptions;
 use std::env;
 use std::path::{Path, PathBuf};
+use glob::glob;
 use std::process::Command;
 
-fn main() {
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+static SOURCE_DIRECTORY: &str = "libpg_query";
+static LIBRARY_NAME: &str = "pg_query";
 
+
+fn main() -> Result<(), Box<dyn std::error::Error>>  {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let build_path = Path::new(".").join("c_libs").join(SOURCE_DIRECTORY);
+    let out_header_path = out_dir.join(LIBRARY_NAME).with_extension("h");
+    let target = env::var("TARGET").unwrap();
+
+
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
     println!("cargo:rustc-link-lib=static=pg_query");
 
-    let system_path = env::var("LIBPG_QUERY_PATH").map(PathBuf::from);
-    let header_path = if let Ok(system_path) = system_path {
-        build_from_system(&system_path);
-        system_path.join("include/pg_query.h").display().to_string()
-    } else {
-        build_from_source(&out_path);
-        out_path
-            .join("libpg_query/pg_query.h")
-            .display()
-            .to_string()
-    };
+    // Copy the relevant source files to the OUT_DIR
+    let source_paths = vec![
+        build_path.join("pg_query").with_extension("h"),
+        build_path.join("Makefile"),
+        build_path.join("src"),
+        build_path.join("protobuf"),
+        build_path.join("vendor"),
+    ];
 
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header(header_path)
-        // Finish the builder and generate the bindings.
+    let copy_options = CopyOptions { overwrite: true, ..CopyOptions::default() };
+
+    fs_extra::copy_items(&source_paths, &out_dir, &copy_options)?;
+
+
+    // Compile the C library.
+    let mut build = cc::Build::new();
+    build
+        .files(glob(out_dir.join("src/*.c").to_str().unwrap()).unwrap().map(|p| p.unwrap()))
+        .files(glob(out_dir.join("src/postgres/*.c").to_str().unwrap()).unwrap().map(|p| p.unwrap()))
+        .file(out_dir.join("vendor/protobuf-c/protobuf-c.c"))
+        .file(out_dir.join("vendor/xxhash/xxhash.c"))
+        .file(out_dir.join("protobuf/pg_query.pb-c.c"))
+        .include(out_dir.join("."))
+        .include(out_dir.join("./vendor"))
+        .include(out_dir.join("./src/postgres/include"))
+        .include(out_dir.join("./src/include"))
+        .warnings(false); // Avoid unnecessary warnings, as they are already considered as part of libpg_query development
+    if env::var("PROFILE").unwrap() == "debug" || env::var("DEBUG").unwrap() == "1" {
+        build.define("USE_ASSERT_CHECKING", None);
+    }
+    if target.contains("windows") {
+        build.include(out_dir.join("./src/postgres/include/port/win32"));
+        if target.contains("msvc") {
+            build.include(out_dir.join("./src/postgres/include/port/win32_msvc"));
+        }
+    }
+    build.compile(LIBRARY_NAME);
+
+    // Generate bindings for Rust
+    bindgen::Builder::default()
+        .header(out_header_path.to_str().ok_or("Invalid header path")?)
         .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .map_err(|_| "Unable to generate bindings")?
+        .write_to_file(out_dir.join("bindings.rs"))?;
+        Ok(())
 }
 
 fn build_from_system(system_path: &Path) {
@@ -42,23 +73,23 @@ fn build_from_system(system_path: &Path) {
     );
 }
 
-fn build_from_source(out_path: &Path) {
+fn build_from_source(out_dir: &Path) {
     run_command(
         "cp",
         &[
             "-r",
             "./c_libs/libpg_query",
-            &out_path.display().to_string(),
+            &out_dir.display().to_string(),
         ],
         None,
     );
 
-    let make_dir = format!("{}/libpg_query", out_path.display());
+    let make_dir = format!("{}/libpg_query", out_dir.display());
     run_command("make", &[], Some(make_dir));
 
     println!(
         "cargo:rustc-link-search=native={}",
-        out_path.join("libpg_query").display()
+        out_dir.join("libpg_query").display()
     );
 }
 
